893db580b31423ee25b827cc62947beb3898fe6d
larry-qian <104049440+larry-qian@users.noreply.github.com>
2024-11-09 00:50:07 -0600

    Add more features to game
    Game now has an incrementing money counter, with the ability to manually add money by clicking the button. However, this progress does not save over reloads of the page. That will be added in later commits.

--- basegame.js
+++ basegame.js
@@ -1,8 +1,15 @@
+//numerical variables
 let money = 0;
 let tokens = 0;
 let prestiges = 0;
 let ascends = 0;
+let tickinterval = 1000;
+let manupgrade = 0;
+
+//variables holding elements of page
 let notices = document.querySelector('.notices');
+let moneycount = document.getElementById('moneycontainer');
+let manualbutton = document.getElementById('manbutton');
 
 function prestige() {
     money = 0;
@@ -16,6 +23,30 @@ function prestige() {
     `
 }
 
-function play() {
-    
+function manualclick() {
+    if(manupgrade == 0) {
+        money += 1;
+    }
+    moneycount.innerHTML = `
+    <h2 class="centertext">${money}</h2>
+    `
 }
+
+function updateMoney() {
+    if(prestiges == 0 && ascends == 0) {
+        money += 1;
+    }else if(prestiges != 0 && ascends == 0) {
+        money += 1 * (1.1 * prestiges);
+    }else if(prestiges != 0 && ascends != 0) {
+        money += 1 ** (1.25 * ascends)
+    }else {
+        money += (1 * (1.1 * prestiges)) ** (1.25 * ascends)
+    }
+    moneycount.innerHTML = `
+    <h2 class="centertext">${money}</h2>
+    `
+}
+
+setInterval(updateMoney, tickinterval);
+
+manualbutton.addEventListener('click', manualclick);
